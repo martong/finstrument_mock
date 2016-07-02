93f0709149a2e14689110074753b65c50734271f
Gabor Marton <martongabesz@gmail.com>
2016-07-02 17:46:38 +0200

    Add extern C __hook

--- example/hook.cpp
+++ example/hook.cpp
@@ -1,6 +1,11 @@
 #include <cstdio>
-int hook(void *callee) {
+
+extern "C" {
+
+int __hook(void *callee) {
     printf("Hello finstrument_mock\n");
     printf("callee: %p\n", callee);
     return 1;
+}
+
 }
