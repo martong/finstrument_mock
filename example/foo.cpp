dbffcb09bbafc68e3974975593e311d7969de50c
Gabor Marton <martongabesz@gmail.com>
2016-07-02 16:45:57 +0200

    Add callee's address as an arg to hook

--- example/foo.cpp
+++ example/foo.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
 void foo() {
     printf("foo called\n");
+    printf("foo's address: %p\n", foo);
 }
--- example/hook.cpp
+++ example/hook.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
-int hook() {
+int hook(void *callee) {
     printf("Hello finstrument_mock\n");
+    printf("callee: %p\n", callee);
     return 1;
 }
