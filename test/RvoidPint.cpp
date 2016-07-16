785582f8049f2779ba3ca57043593ce56ffaef91
Gabor Marton <martongabesz@gmail.com>
2016-07-17 00:29:01 +0200

    Add test for lvalue

--- test/RvoidPint.cpp
+++ test/RvoidPint.cpp
@@ -11,3 +11,7 @@ void RvoidPint_2(int a) {
 void RvoidPint_3(int a) {
     ::ftest::called.insert(reinterpret_cast<char*>(RvoidPint_3));
 }
+
+void RvoidPint_4(int a) {
+    ::ftest::called.insert(reinterpret_cast<char*>(RvoidPint_4));
+}
--- test/fake_targets/RvoidPintCaller.cpp
+++ test/fake_targets/RvoidPintCaller.cpp
@@ -1,16 +1,26 @@
-//int square(int a) { return a*a; }
-
 void RvoidPint(int);
 void RvoidPint_2(int);
 void RvoidPint_3(int);
+void RvoidPint_4(int);
 
 void RvoidPintCaller() {
   RvoidPint(13);
   RvoidPint_2(15);
-  //RvoidPint(square(3));
 }
 
 void RvoidPintCaller_callsTwice() {
   RvoidPint_3(13);
   RvoidPint_3(42);
+}
+
+// NOTE, we can't handle return values, yet
+//static int square(int a) { return a*a; }
+static void square(int a, int& result) { result = a*a; }
+
+void RvoidPintCaller_lvalue(int a) {
+    int res;
+    square(a, res);
+    // NOTE, we can't handle return values, yet
+    //RvoidPint_4(square(a));
+    RvoidPint_4(res);
 }
