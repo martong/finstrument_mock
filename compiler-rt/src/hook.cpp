4f6e1d9aef302718c16a557ff5be9f094fe6490b
Gabor Marton <martongabesz@gmail.com>
2016-07-20 20:50:39 +0200

    Use only one hook

--- compiler-rt/src/hook.cpp
+++ compiler-rt/src/hook.cpp
@@ -9,16 +9,11 @@ namespace fake {
 
 extern "C" {
 
-int __fake_subject_hook(void *callee) {
-    printf("__fake_subject_hook; callee: %p\n", callee);
-    return ::fake::subs.count(reinterpret_cast<char*>(callee)) > 0;
-    //return 1;
-}
-
 void* __fake_hook_map(void *callee) {
     printf("__fake_hook; callee: %p\n", callee);
-    char* fake_fun = ::fake::subs.at(reinterpret_cast<char*>(callee));
-    return fake_fun;
+    auto it = ::fake::subs.find(reinterpret_cast<char*>(callee));
+    if (it == ::fake::subs.end()) { return nullptr; }
+    else { return it->second; }
 }
 
 } // extern "C"
