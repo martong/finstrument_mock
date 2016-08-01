e05cd6c16d44c0dd1c3a2e798455296d62cdc2c8
Gabor Marton <martongabesz@gmail.com>
2016-08-02 00:37:19 +0200

    Add rvalue test

--- test/RAggrTest.cpp
+++ test/RAggrTest.cpp
@@ -12,13 +12,25 @@ Aggr fake_RAggr(Aggr pa){
     return {pa.a * 2, pa.b * 2};
 }
 
-TEST_F(FooFixture, RAggr) {
+TEST_F(FooFixture, RAggrLvalue) {
     ::fake::subs.insert(
         {address(RAggr), address(fake_RAggr)});
     Aggr result = RAggrCaller(Aggr{13, 17});
 
     EXPECT_EQ(result.a, 26);
     EXPECT_EQ(result.b, 34);
+
+    EXPECT_EQ(::ftest::called.count(address(RAggr)), 0);
+    EXPECT_EQ(::ftest::called.count(address(fake_RAggr)), 1);
+    EXPECT_EQ(::ftest::called.count(address(RAggr_2)), 1);
+}
+
+TEST_F(FooFixture, RAggrRvalue) {
+    ::fake::subs.insert(
+        {address(RAggr), address(fake_RAggr)});
+
+    EXPECT_EQ(RAggrCaller(Aggr{13, 17}).a, 26);
+    EXPECT_EQ(RAggrCaller(Aggr{13, 17}).b, 34);
 
     EXPECT_EQ(::ftest::called.count(address(RAggr)), 0);
     EXPECT_EQ(::ftest::called.count(address(fake_RAggr)), 1);
