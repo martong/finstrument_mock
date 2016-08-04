23b8acaded2b965b0cd1300f093f85d46c598723
Gabor Marton <martongabesz@gmail.com>
2016-08-04 23:10:37 +0200

    Add SUBSTITUTE macro

--- compiler-rt/include/hook.hpp
+++ compiler-rt/include/hook.hpp
@@ -5,11 +5,17 @@ namespace fake {
 void clear();
 void insert(std::pair<char*, char*>);
 
-}
-
 template <typename T>
 char *address(T t) {
     // This is the only way to get the absolute address of a member function
     // http://stackoverflow.com/questions/8121320/get-memory-address-of-member-function
     return (char *&)(t);
 }
+
+
+}
+
+#define SUBSTITUTE(src, dst)                                              \
+    do {                                                                  \
+        ::fake::insert({::fake::address((src)), ::fake::address((dst))}); \
+    } while (0)
