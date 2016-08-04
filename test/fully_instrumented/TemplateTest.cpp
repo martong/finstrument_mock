1695495e2ff799a9672fb212ff7aecd225574066
Gabor Marton <martongabesz@gmail.com>
2016-08-05 00:40:35 +0200

    Cleanup

--- test/fully_instrumented/TemplateTest.cpp
+++ test/fully_instrumented/TemplateTest.cpp
@@ -1,55 +1,62 @@
 #include "FooFixture.hpp"
 
-#include "Template.hpp"
-
-int fake_bar_mem_fun(TemplateS<int>* self, int p) {
-  return p * 3;
-}
-
+template <typename T>
+struct TemplateS {
+    int foo(int p) { return bar(p); }
+    int bar(int p) { return p; }
+};
+int fake_bar_mem_fun(TemplateS<int>* self, int p) { return p * 3; }
 TEST_F(FooFixture, ClassT) {
-  SUBSTITUTE(&TemplateS<int>::bar, &fake_bar_mem_fun);
-  TemplateS<int> t;
-  auto res = t.foo(13);
-  EXPECT_EQ(res, 39);
+    SUBSTITUTE(&TemplateS<int>::bar, &fake_bar_mem_fun);
+    TemplateS<int> t;
+    auto res = t.foo(13);
+    EXPECT_EQ(res, 39);
 }
 
 template <typename T>
 T FunTemp(T t) {
-  return t;
+    return t;
 }
-int fake_FunTemp(int p) {
-  return p * 3;
-}
+int fake_FunTemp(int p) { return p * 3; }
 TEST_F(FooFixture, FunT) {
-  SUBSTITUTE(&FunTemp<int>, &fake_FunTemp);
-  int p = 13;
-  auto res = FunTemp(p);
-  EXPECT_EQ(res, 39);
+    SUBSTITUTE(&FunTemp<int>, &fake_FunTemp);
+    int p = 13;
+    auto res = FunTemp(p);
+    EXPECT_EQ(res, 39);
 }
 
 struct Class {
-template <typename T>
-T FunTemp(T t) {
-  return t;
-}
+    template <typename T>
+    T FunTemp(T t) {
+        return t;
+    }
 };
-int fake_Class_mf(Class*, int p) {
-  return p * 3;
-}
+int fake_Class_mf(Class*, int p) { return p * 3; }
 TEST_F(FooFixture, ClassFunT) {
-  SUBSTITUTE(&Class::FunTemp<int>, &fake_Class_mf);
-  Class c;
-  int p = 13;
-  auto res = c.FunTemp(p);
-  EXPECT_EQ(res, 39);
+    SUBSTITUTE(&Class::FunTemp<int>, &fake_Class_mf);
+    Class c;
+    int p = 13;
+    auto res = c.FunTemp(p);
+    EXPECT_EQ(res, 39);
 }
 
+template <typename T>
+struct TemplateS2 {
+    template <typename U>
+    U fooT(U p) {
+        return barT(p);
+    }
+    template <typename U>
+    U barT(U p) {
+        return p;
+    }
+};
 TEST_F(FooFixture, ClassTFunT) {
-  SUBSTITUTE(&TemplateS<int>::barT<int>, &fake_bar_mem_fun);
-  TemplateS<int> t;
-  int p = 13;
-  auto res = t.barT(p);
-  EXPECT_EQ(res, 39);
-  res = t.fooT(p);
-  EXPECT_EQ(res, 39);
+    SUBSTITUTE(&TemplateS2<int>::barT<int>, &fake_bar_mem_fun);
+    TemplateS2<int> t;
+    int p = 13;
+    auto res = t.barT(p);
+    EXPECT_EQ(res, 39);
+    res = t.fooT(p);
+    EXPECT_EQ(res, 39);
 }
