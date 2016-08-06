9b126a72078bb7e5480f9bcf7f5e7201b2e4bccf
Gabor Marton <martongabesz@gmail.com>
2016-08-06 13:14:12 +0200

    Add exception test

---
+++ test/fully_instrumented/ExceptionTest.cpp
@@ -0,0 +1,19 @@
+#include "FooFixture.hpp"
+
+namespace {
+    int bar() { return 13; }
+    int foo() {
+        auto result = bar();
+        return result;
+    }
+
+    struct BarE {};
+    int fake_bar() { throw BarE{}; return 15; }
+}
+
+TEST_F(FooFixture, Exception) {
+    EXPECT_NO_THROW(foo());
+
+    SUBSTITUTE(&bar, &fake_bar);
+    EXPECT_THROW(foo(), BarE);
+}
