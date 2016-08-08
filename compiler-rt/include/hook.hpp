03097d47d434400a5608df8e5b877a87ac1b6c84
Gabor Marton <martongabesz@gmail.com>
2016-08-09 00:23:51 +0200

    Support noreturn

--- compiler-rt/include/hook.hpp
+++ compiler-rt/include/hook.hpp
@@ -6,15 +6,39 @@ void clear();
 void insert(char* src, char* dst);
 
 template <typename T>
-char *address(T t) {
-    // This is the only way to get the absolute address of a member function
-    // http://stackoverflow.com/questions/8121320/get-memory-address-of-member-function
-    return (char *&)(t);
+char* address(T t) {
+    /// This is the only way to get the absolute address of a member function
+    /// http://stackoverflow.com/questions/8121320/get-memory-address-of-member-function
+    return (char*&)(t);
 }
 
-}
+} // namespace fake
 
-#define SUBSTITUTE(src, dst)                                              \
-    do {                                                                  \
+#define SUBSTITUTE(src, dst)                                            \
+    do {                                                                \
         ::fake::insert(::fake::address((src)), ::fake::address((dst))); \
+    } while (0)
+
+/// Use this macro for substituting noreturn functions.
+///
+/// Note, be warned, you are going to have UB:
+/// The standard states: The behavior is undefined if the function with this
+/// attribute actually returns.
+///
+/// Note2, for some reason
+/// SUBSTITUTE(&std::exit, &fake_exit) fails because of a strange linker error.
+///   ../compiler-rt/include/hook.hpp:9:7: error: definition with same mangled
+///   name as another definition
+///   char *address(T t) {
+///         ^
+///   ../compiler-rt/include/hook.hpp:9:7: note: previous definition is here
+///   1 error generated.
+///
+///   This is happening always with __dead2 (noreturn) functions in stdlib.h
+///
+///   Bug:
+///   https://llvm.org/bugs/show_bug.cgi?id=28909
+#define SUBSTITUTE_NORETURN(src, dst)                   \
+    do {                                                \
+        ::fake::insert((char*)((src)), (char*)((dst))); \
     } while (0)
---
+++ test/fully_instrumented/NoReturn.cpp
@@ -0,0 +1,94 @@
+#include "hook.hpp"
+#include "FooFixture.hpp"
+#include <cstdlib>
+#include <string>
+
+struct NoReturn : FooFixture {};
+
+namespace {
+[[noreturn]] void foo(int) { exit(0); };
+void fake_foo(int p) {
+    EXPECT_EQ(p, 13);
+    ::ftest::called.insert(address(&fake_foo));
+}
+void bar() { foo(13); };
+} // unnamed
+TEST_F(NoReturn, foo) {
+    SUBSTITUTE(&foo, &fake_foo);
+    bar();
+    EXPECT_EQ(::ftest::called.count(address(fake_foo)), 1);
+}
+
+void parseCommandLineArgs(int argc, char** argv) {
+    if (argc != 3) {
+        std::exit(1);
+    }
+    if (std::string(argv[1]) != "apple") {
+        std::exit(2);
+    }
+    if (std::string(argv[2]) != "banana") {
+        std::exit(3);
+    }
+}
+
+int exit_code = -1;
+struct Exc {};
+void fake_exit(int ec) {
+    ::exit_code = ec;
+    throw Exc{};
+}
+
+TEST_F(NoReturn, parseCommandLineArgs) {
+    SUBSTITUTE_NORETURN(&std::exit, &fake_exit);
+
+    /// argc mismatch
+    exit_code = -1;
+    try {
+        parseCommandLineArgs(1, nullptr);
+    } catch (Exc&) {
+    }
+    EXPECT_EQ(::exit_code, 1);
+
+    /// first param bad
+    {
+        char a0[] = "prog";
+        char a1[] = "asdf";
+        char a2[] = "asdff";
+        char* argv[3] = {a0, a1, a2};
+        exit_code = -1;
+        try {
+            parseCommandLineArgs(3, argv);
+        } catch (Exc&) {
+        }
+        EXPECT_EQ(::exit_code, 2);
+    }
+
+    /// second param bad
+    {
+        char a0[] = "prog";
+        char a1[] = "apple";
+        char a2[] = "asdff";
+        char* argv[3] = {a0, a1, a2};
+        exit_code = -1;
+        try {
+            parseCommandLineArgs(3, argv);
+        } catch (Exc&) {
+        }
+        EXPECT_EQ(::exit_code, 3);
+    }
+
+    /// all params are good
+    {
+        char a0[] = "prog";
+        char a1[] = "apple";
+        char a2[] = "banana";
+        char* argv[3] = {a0, a1, a2};
+        exit_code = -1;
+        try {
+            parseCommandLineArgs(3, argv);
+        } catch (Exc&) {
+        }
+        /// exit_code should have not changed
+        EXPECT_EQ(::exit_code, -1);
+    }
+}
