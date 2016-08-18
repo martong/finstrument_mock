c1ccc2130b3f8f9bef9aaba9a7670f2dc32e2fd7
Gabor Marton <martongabesz@gmail.com>
2016-08-18 22:22:31 +0200

    Add more constexpr tests

--- test/fully_instrumented/Constexpr.cpp
+++ test/fully_instrumented/Constexpr.cpp
@@ -5,15 +5,20 @@ namespace {
 constexpr int foo(int p) { return p*p; }
 int fake_foo(int p) { return p*p*p; }
 
-int bar(int p) {
+constexpr int bar(int p) {
     return foo(p);
 }
 
 } // unnamed
 
 TEST_F(FooFixture, Constexpr) {
-    static_assert(foo(2) == 4, "");
     SUBSTITUTE(&foo, &fake_foo);
+
     static_assert(foo(2) == 4, "");
-    EXPECT_EQ(bar(2), 8);
+    EXPECT_EQ(foo(2), 8);
+    int p = 2;
+    EXPECT_EQ(foo(p), 8);
+
+    static_assert(bar(2) == 4, "");
+    EXPECT_EQ(bar(p), 8);
 }
