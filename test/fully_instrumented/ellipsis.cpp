073227f1a6acbf8c73275a962d51889206553664
Gabor Marton <martongabesz@gmail.com>
2016-11-20 21:19:47 +0100

    Add test for variadic arguments (ellipsis)

---
+++ test/fully_instrumented/ellipsis.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include "FooFixture.hpp"
+
+int add_nums(int count, ...)
+{
+    int result = 0;
+    va_list args;
+    va_start(args, count);
+    for (int i = 0; i < count; ++i) {
+        result += va_arg(args, int);
+    }
+    va_end(args);
+    return result;
+}
+
+int mul_nums(int count, ...)
+{
+    int result = 1;
+    va_list args;
+    va_start(args, count);
+    for (int i = 0; i < count; ++i) {
+        result *= va_arg(args, int);
+    }
+    va_end(args);
+    return result;
+}
+
+TEST_F(FooFixture, Ellipsis) {
+    EXPECT_EQ(add_nums(4, 1, 2, 3, 4), 10);
+    EXPECT_EQ(mul_nums(4, 1, 2, 3, 4), 24);
+    SUBSTITUTE(&add_nums, &mul_nums);
+    EXPECT_EQ(add_nums(4, 1, 2, 3, 4), 24);
+}
