e30e639688e550e4552eae87cb0cef23ae25d845
Gabor Marton <martongabesz@gmail.com>
2016-11-20 20:11:23 +0100

    Add more tests for dynamic types

--- test/fully_instrumented/DynamicType.cpp
+++ test/fully_instrumented/DynamicType.cpp
@@ -1,40 +1,224 @@
 #include "FooFixture.hpp"
 
-namespace {
+struct DynamicType : FooFixture {};
+
+namespace Basic {
+enum class Values : int { B, D, FB, FD };
 struct B {
-    virtual int foo2(int p) { return p; }
-    virtual int foo(int p) { return p; }
+    virtual Values foo(int p) { return Values::B; }
 };
 struct D : B {
-    virtual int foo2(int p) override { return p + p; }
-    virtual int foo(int p) override { return p + p; }
+    virtual Values foo(int p) override { return Values::D; }
 };
-int B_fake_foo(B*, int p) { return p + p + p; }
-int D_fake_foo(D*, int p) { return p + p + p + p; }
-} // unnamed
+Values B_fake_foo(B*, int p) { return Values::FB; }
+Values D_fake_foo(D*, int p) { return Values::FD; }
+}
 
-TEST_F(FooFixture, DynamicType) {
+TEST_F(DynamicType, Basic_D) {
+    using Basic::B;
+    using Basic::D;
+    using Basic::D_fake_foo;
+    using Basic::Values;
+
     B* dummy = new D;
     SUBSTITUTE_VIRTUAL(&D::foo, dummy, &D_fake_foo);
     {
         B* b0 = new B;
-        EXPECT_EQ(b0->foo(1), 1); // As without mock san
+        EXPECT_EQ(b0->foo(1), Values::B);
     }
     {
         B* b1 = new D;
-        EXPECT_EQ(b1->foo(1), 4);
+        EXPECT_EQ(b1->foo(1), Values::FD);
     }
 }
 
-TEST_F(FooFixture, DynamicType2) {
+TEST_F(DynamicType, Basic_B) {
+    using Basic::B;
+    using Basic::D;
+    using Basic::B_fake_foo;
+    using Basic::Values;
+
     B* dummy = new B;
     SUBSTITUTE_VIRTUAL(&B::foo, dummy, &B_fake_foo);
     {
         B* b0 = new B;
-        EXPECT_EQ(b0->foo(1), 3);
+        EXPECT_EQ(b0->foo(1), Values::FB);
     }
     {
         B* b1 = new D;
-        EXPECT_EQ(b1->foo(1), 2); // As w/o mock san
+        EXPECT_EQ(b1->foo(1), Values::D);
     }
 }
+
+namespace InheritanceChain {
+enum class Values : int { B, C, D, FB, FC, FD };
+struct B {
+    virtual Values foo(int p) { return Values::B; }
+};
+struct C : B {
+    virtual Values foo(int p) { return Values::C; }
+};
+struct D : C {
+    virtual Values foo(int p) override { return Values::D; }
+};
+Values B_fake_foo(B*, int p) { return Values::FB; }
+Values C_fake_foo(C*, int p) { return Values::FC; }
+Values D_fake_foo(D*, int p) { return Values::FD; }
+}
+
+TEST_F(DynamicType, InheritanceChain_D) {
+    using InheritanceChain::Values;
+    using InheritanceChain::B;
+    using InheritanceChain::C;
+    using InheritanceChain::D;
+    using InheritanceChain::D_fake_foo;
+
+    B* dummy = new D;
+    SUBSTITUTE_VIRTUAL(&D::foo, dummy, &D_fake_foo);
+    {
+        B* b0 = new B;
+        EXPECT_EQ(b0->foo(1), Values::B);
+    }
+    {
+        C* b0 = new C;
+        EXPECT_EQ(b0->foo(1), Values::C);
+    }
+    {
+        B* b1 = new D;
+        EXPECT_EQ(b1->foo(1), Values::FD);
+    }
+}
+
+TEST_F(DynamicType, InheritanceChain_C) {
+    using InheritanceChain::Values;
+    using InheritanceChain::B;
+    using InheritanceChain::C;
+    using InheritanceChain::D;
+    using InheritanceChain::C_fake_foo;
+
+    B* dummy = new C;
+    SUBSTITUTE_VIRTUAL(&C::foo, dummy, &C_fake_foo);
+    {
+        B* b0 = new B;
+        EXPECT_EQ(b0->foo(1), Values::B);
+    }
+    {
+        C* b0 = new C;
+        EXPECT_EQ(b0->foo(1), Values::FC);
+    }
+    {
+        B* b1 = new D;
+        EXPECT_EQ(b1->foo(1), Values::D);
+    }
+}
+
+TEST_F(DynamicType, InheritanceChain_B) {
+    using InheritanceChain::Values;
+    using InheritanceChain::B;
+    using InheritanceChain::C;
+    using InheritanceChain::D;
+    using InheritanceChain::B_fake_foo;
+
+    B* dummy = new B;
+    SUBSTITUTE_VIRTUAL(&B::foo, dummy, &B_fake_foo);
+    {
+        B* b0 = new B;
+        EXPECT_EQ(b0->foo(1), Values::FB);
+    }
+    {
+        C* b0 = new C;
+        EXPECT_EQ(b0->foo(1), Values::C);
+    }
+    {
+        B* b1 = new D;
+        EXPECT_EQ(b1->foo(1), Values::D);
+    }
+}
+
+namespace MultipleInheritance {
+enum class Values : int { B, C, D, FB, FC, FD };
+struct B {
+    virtual Values foo(int p) { return Values::B; }
+};
+struct C {
+    virtual Values foo(int p) { return Values::C; }
+};
+struct D : B, C {
+    virtual Values foo(int p) override { return Values::D; }
+};
+Values B_fake_foo(B*, int p) { return Values::FB; }
+Values C_fake_foo(C*, int p) { return Values::FC; }
+Values D_fake_foo(D*, int p) { return Values::FD; }
+}
+
+
+TEST_F(DynamicType, MultipleInheritance_D) {
+    using MultipleInheritance::Values;
+    using MultipleInheritance::B;
+    using MultipleInheritance::C;
+    using MultipleInheritance::D;
+    using MultipleInheritance::D_fake_foo;
+
+    B* dummy = new D;
+    SUBSTITUTE_VIRTUAL(&D::foo, dummy, &D_fake_foo);
+    {
+        B* b0 = new B;
+        EXPECT_EQ(b0->foo(1), Values::B);
+    }
+    {
+        C* b0 = new C;
+        EXPECT_EQ(b0->foo(1), Values::C);
+    }
+    {
+        B* b1 = new D;
+        EXPECT_EQ(b1->foo(1), Values::FD);
+    }
+}
+
+TEST_F(DynamicType, MultipleInheritance_C) {
+    using MultipleInheritance::Values;
+    using MultipleInheritance::B;
+    using MultipleInheritance::C;
+    using MultipleInheritance::D;
+    using MultipleInheritance::C_fake_foo;
+
+    C* dummy = new C;
+    SUBSTITUTE_VIRTUAL(&C::foo, dummy, &C_fake_foo);
+    {
+        B* b0 = new B;
+        EXPECT_EQ(b0->foo(1), Values::B);
+    }
+    {
+        C* b0 = new C;
+        EXPECT_EQ(b0->foo(1), Values::FC);
+    }
+    {
+        B* b1 = new D;
+        EXPECT_EQ(b1->foo(1), Values::D);
+    }
+}
+
+TEST_F(DynamicType, MultipleInheritance_B) {
+    using MultipleInheritance::Values;
+    using MultipleInheritance::B;
+    using MultipleInheritance::C;
+    using MultipleInheritance::D;
+    using MultipleInheritance::B_fake_foo;
+
+    B* dummy = new B;
+    SUBSTITUTE_VIRTUAL(&B::foo, dummy, &B_fake_foo);
+    {
+        B* b0 = new B;
+        EXPECT_EQ(b0->foo(1), Values::FB);
+    }
+    {
+        C* b0 = new C;
+        EXPECT_EQ(b0->foo(1), Values::C);
+    }
+    {
+        B* b1 = new D;
+        EXPECT_EQ(b1->foo(1), Values::D);
+    }
+}
+
+// TODO virtual inheritance
