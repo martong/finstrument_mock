5a6bb2db781bd7e2783676037444ecf58069bfdf
Gabor Marton <martongabesz@gmail.com>
2017-06-11 19:12:46 +0200

    Use stderr and Printf

--- compiler-rt/src/hook.cpp
+++ compiler-rt/src/hook.cpp
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <unistd.h> // _SC_PAGESIZE
 #include <cassert>
+#include <cstdarg>
 #include <sys/mman.h> // mmap
 
 #include "hook.hpp"
@@ -52,14 +53,21 @@ static const u64 kOSXLowShadowOffset = kOSXLowMemEnd + 1;
 #define kHighShadowBeg MEM_TO_SHADOW(kHighMemBeg)
 #define kHighShadowEnd (MEM_TO_SHADOW(kHighMemEnd + 1) - 1)
 
+void Printf(const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  fprintf(stderr, format, args);
+  va_end(args);
+}
+
 void PrintAddressSpaceLayout() {
-  printf("|| `[%p, %p]` || HighMem    ||\n", (void *)kHighMemBeg,
+  Printf("|| `[%p, %p]` || HighMem    ||\n", (void *)kHighMemBeg,
          (void *)kHighMemEnd);
-  printf("|| `[%p, %p]` || HighShadow ||\n", (void *)kHighShadowBeg,
+  Printf("|| `[%p, %p]` || HighShadow ||\n", (void *)kHighShadowBeg,
          (void *)kHighShadowEnd);
-  printf("|| `[%p, %p]` || LowShadow  ||\n", (void *)kLowShadowBeg,
+  Printf("|| `[%p, %p]` || LowShadow  ||\n", (void *)kLowShadowBeg,
          (void *)kLowShadowEnd);
-  printf("|| `[%p, %p]` || LowMem     ||\n", (void *)kLowMemBeg,
+  Printf("|| `[%p, %p]` || LowMem     ||\n", (void *)kLowMemBeg,
          (void *)kLowMemEnd);
 }
 
@@ -94,7 +102,7 @@ int Munmap(uptr fixed_addr, uptr size) {
   int res = internal_munmap((void *)(fixed_addr & ~(PageSize - 1)),
                             RoundUpTo(size, PageSize));
   if (res != 0)
-    printf("ERROR: %s failed to "
+    Printf("ERROR: %s failed to "
            "deallocate 0x%llx (%zd) bytes at address %llx (errno: %d)\n",
            "MockSanitizer", size, size, fixed_addr, res);
   return res;
@@ -112,7 +120,7 @@ void *MmapFixedNoReserve(uptr fixed_addr, uptr size, const char *name) {
                          flags, fd, 0);
   int reserrno;
   if (internal_iserror(p, &reserrno))
-    printf("ERROR: %s failed to "
+    Printf("ERROR: %s failed to "
            "allocate 0x%llx (%zd) bytes at address %llx (errno: %d)\n",
            "MockSanitizer", size, size, fixed_addr, reserrno);
   return (void *)p;
@@ -141,7 +149,7 @@ void ReserveShadowMemoryRange(uptr beg, uptr end, const char *name) {
   uptr size = end - beg + 1;
   void *res = MmapFixedNoReserve(beg, size, name);
   if (res != (void *)beg) {
-    printf("ReserveShadowMemoryRange failed while trying to map 0x%llx bytes. "
+    Printf("ReserveShadowMemoryRange failed while trying to map 0x%llx bytes. "
            "Perhaps you're using ulimit -v\n",
            size);
     abort();
@@ -154,7 +162,7 @@ void ReleaseShadowMemoryRange(uptr beg, uptr end) {
 }
 
 __attribute__((constructor)) void mocksan_init() {
-  printf("mocksan_init\n");
+  Printf("mocksan_init\n");
   PrintAddressSpaceLayout();
   ReserveShadowMemoryRange(kLowShadowBeg, kLowShadowEnd, "low shadow");
   ReserveShadowMemoryRange(kHighShadowBeg, kHighShadowEnd, "high shadow");
@@ -185,7 +193,7 @@ inline bool AddrIsInMem(uptr a) {
 namespace fake {
 
 void clear() {
-  printf("mocksan clear()\n");
+  Printf("mocksan clear()\n");
   ReleaseShadowMemoryRange(kLowShadowBeg, kLowShadowEnd);
   ReleaseShadowMemoryRange(kHighShadowBeg, kHighShadowEnd);
   ReserveShadowMemoryRange(kLowShadowBeg, kLowShadowEnd, "low shadow");
@@ -193,11 +201,11 @@ void clear() {
 }
 
 void insert(char *src, char *dst) {
-  printf("insert; src, dst: %p, %p\n", src, dst);
+  Printf("insert; src, dst: %p, %p\n", src, dst);
   assert(AddrIsInMem((uptr)src));
   uptr shadow = MemToShadow((uptr)src);
   char **shadowPtr = reinterpret_cast<char **>(shadow);
-  printf("insert; shadowPtr: %p\n", shadowPtr);
+  Printf("insert; shadowPtr: %p\n", shadowPtr);
   *shadowPtr = dst;
 }
 
