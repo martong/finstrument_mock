a37708affbc441830b188189c40eba5edde73f1d
Gabor Marton <martongabesz@gmail.com>
2017-06-18 20:21:07 +0200

    Use list for keep sorting

--- measure/eval_penalty_in_one_plot.py
+++ measure/eval_penalty_in_one_plot.py
@@ -9,7 +9,8 @@ import argparse
 
 
 def plot(results):
-    penalty_names = results.itervalues().next().keys()
+    penalty_names = results[0][1].keys()
+    print penalty_names
     ind = np.arange(len(penalty_names)) + .5
     colors = ['r', 'g', 'b', 'y', 'c']
     #colors = ['w', 'w', 'w', 'w', 'w']
@@ -19,9 +20,10 @@ def plot(results):
     height = 0.8 / len(results)  # keep 0.2 for place between each penalty
     # with align='edge' -0.5 offset would put to the bottom of the penalty
     offset = -0.4
-    for penalty_name, result in results.iteritems():
+    for penalty_name, result in results:
         plt.barh(ind + offset + fi, result.values(), align='edge',
-                 height=height, color=colors[i], hatch=hatches[i], label=penalty_name)
+                 height=height, color=colors[i], hatch=hatches[i],
+                 label=penalty_name)
         fi = fi + height
         i = i + 1
 
@@ -30,7 +32,7 @@ def plot(results):
     plt.legend(loc='upper right', shadow=True, fontsize='medium')
 
     #plt.title(name)
-    # plt.xlabel('seconds')
+    #plt.xlabel('seconds')
     #plt.ylabel('compiler setup')
 
     plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
@@ -60,7 +62,7 @@ def parse_result_file(filename):
 
 
 def plot_all_results():
-    results = {}
+    results = []
     for dir in [
         'O2',
         'O0',
@@ -73,7 +75,7 @@ def plot_all_results():
         result = parse_result_file(result_file)
         compiler_setup = "-" + result_file.split(
             "/")[0].replace("__", " -").replace("_mock", "=mock")
-        results[compiler_setup] = result
+        results.append((compiler_setup, result))
     plot(results)
 
 plot_all_results()
