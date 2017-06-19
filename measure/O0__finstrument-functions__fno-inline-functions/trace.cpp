a07ac399604031156d603a284a91105d88d5d3de
Gabor Marton <martongabesz@gmail.com>
2017-06-19 23:12:44 +0200

    Add new measurement with new compiler setups

---
+++ measure/O0__finstrument-functions__fno-inline-functions/trace.cpp
@@ -0,0 +1,15 @@
+extern "C" {
+
+void __cyg_profile_func_enter(void *this_fn, void *call_site)
+    __attribute__((no_instrument_function));
+void __cyg_profile_func_enter(void *this_fn, void *call_site) {
+    // printf("ENTER: %p, from %p\n", this_fn, call_site);
+} /* __cyg_profile_func_enter */
+
+void __cyg_profile_func_exit(void *this_fn, void *call_site)
+    __attribute__((no_instrument_function));
+void __cyg_profile_func_exit(void *this_fn, void *call_site) {
+    // printf("EXIT:  %p, from %p\n", this_fn, call_site);
+} /* __cyg_profile_func_enter */
+
+}
